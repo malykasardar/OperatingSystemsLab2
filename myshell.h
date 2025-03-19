3390045401c832b4f437bda5b80b1a8f6f031929
Malyka Sardar <121200777+malykasardar@users.noreply.github.com>
2025-03-19 14:26:46 -0400

    Create myshell.h

---
+++ myshell.h
@@ -0,0 +1,8 @@
+#ifndef MYSHELL_H
+#define MYSHELL_H
+#include <stdio.h> // Standard I/O functions
+#include <stdlib.h> // Standard library for functions like malloc
+#include <string.h> // String handling functions
+#include <unistd.h> // Provides access to the POSIX operating system API
+void change_directory(char *path); // Declaration of 'cd' function
+#endif
