0b8345d28207e8efd48b6bb30fc78d01495ed316
Malyka Sardar <121200777+malykasardar@users.noreply.github.com>
2025-03-19 21:36:04 -0400

    Update utility.c

--- utility.c
+++ utility.c
@@ -3,17 +3,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
+
 void change_directory(char *path) {
-if (path == NULL) {
-char cwd[1024];
-if (getcwd(cwd, sizeof(cwd)) != NULL) {
-printf("%s\n", cwd);
-} else {
-perror("getcwd() error");
+    if (path == NULL) {
+        // Print current directory if no path is provided
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("%s\n", cwd);
+        } else {
+            perror("getcwd");
+        }
+    } else {
+        // Change directory to the specified path
+        if (chdir(path) != 0) {
+            perror("myshell");
+        }
+    }
 }
-} else {
-if (chdir(path) != 0) {
-perror("myshell");
+
+void list_directory(char *path) {
+    if (path == NULL) {
+        path = "."; // Default to current directory
+    }
+
+    DIR *dir = opendir(path);
+    if (dir == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
+    closedir(dir);
 }
+
+void list_environment() {
+    extern char **environ;
+    for (char **env = environ; *env != NULL; env++) {
+        printf("%s\n", *env);
+    }
 }
+
+void echo_comment(char *comment) {
+    if (comment != NULL) {
+        printf("%s\n", comment);
+    }
+}
+
+void pause_shell() {
+    printf("Press Enter to continue...");
+    getchar(); // Wait for Enter key
 }
